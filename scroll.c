b437ec1531153086b66016e4fc79297b943688d5
noufalc <muhammednoufalc@gmail.com>
2015-04-09 08:10:26 -0400

    two letters

--- scroll.c
+++ scroll.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-unsigned short int charecterA[8] = {0x0e, 0x11, 0x11, 0x11, 0x1f, 0x11, 0x11, 0x11};
+
+unsigned short int charecterA[][8] = {{0x0e, 0x11, 0x11, 0x11, 0x1f, 0x11, 0x11, 0x11}, {0x0e, 0x09, 0x09, 0x0e, 0x09, 0x09, 0x09, 0x0e}};
 
 int send_data(unsigned int data)
 {
@@ -24,11 +25,12 @@ int main (int argc, char **argv)
 	system("clear");
 	for (i = 0; i < 8; i++)
 		display_buffer[i] = 0x0000;
+	for (i = 0; i < 2; i++)
 	for (scroll = 0; scroll < 8; scroll++)
 	{
 		for (shift_amount = 0; shift_amount < 8; shift_amount++)
 		{
-			temp = charecterA[shift_amount];
+			temp = charecterA[i][shift_amount];
 			display_buffer[shift_amount] = (display_buffer[shift_amount] << scroll) | (temp >> (8 - scroll));
 			send_data(display_buffer[shift_amount]);
 		}
