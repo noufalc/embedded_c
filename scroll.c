e6f0ff2e6805c2c118577e0eba823e1a5513dacf
noufalc <muhammednoufalc@gmail.com>
2015-04-10 06:43:15 -0400

    scrolling fixed

--- scroll.c
+++ scroll.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned char charecterA[][8] = {{0x0e, 0x11, 0x11, 0x11, 0x1f, 0x11, 0x11, 0x11}, {0x0e, 0x09, 0x09, 0x0e, 0x09, 0x09, 0x09, 0x0e}};
+unsigned char charecterA[][8] = {{0x0e, 0x11, 0x11, 0x11, 0x1f, 0x11, 0x11, 0x11}, {0x0e, 0x09, 0x09, 0x0e, 0x09, 0x09, 0x09, 0x0e}, {0x11, 0x19, 0x15, 0x13}};
 
 int send_data(unsigned int data)
 {
@@ -28,16 +28,26 @@ int main (int argc, char **argv)
 	for (i = 0; i < 8; i++)
 		display_buffer[i] = 0x0000;
 	for (i = 0; i < 2; i++)
-	for (scroll = 1; scroll < 8; scroll++)
+	for (scroll = 4; scroll < 9; scroll++)
 	{
 		for (shift_amount = 0; shift_amount < 8; shift_amount++)
 		{
 			temp = charecterA[i][shift_amount];
-			display_buffer[shift_amount] = (display_buffer[shift_amount] << scroll) | (temp >> (8 - scroll - 1));
+			display_buffer[shift_amount] = (display_buffer[shift_amount] << 1) | (temp >> (8 - scroll));
 			send_data(display_buffer[shift_amount]);
 		}
-	system("sleep 1");
+	system("sleep 0.6");
 	system("clear");
 	}
+	for (i = 0; i < 16; i++)
+	{
+		for (shift_amount = 0; shift_amount < 8; shift_amount++)
+		{
+			display_buffer[shift_amount] = display_buffer[shift_amount] << 1;
+			send_data(display_buffer[shift_amount]);
+		}
+		system("sleep 0.6");
+		system("clear");
+	}	
 	return 0;
 }
